50d3db494818452e30dc5c84606abfdb708f3f11
Edward Matthew <edward.kurniawan@binus.ac.id>
2022-03-26 11:42:30 +0700

    added string literal validation

--- Compilation Techniques/forums_lexer/lexer.py
+++ Compilation Techniques/forums_lexer/lexer.py
@@ -18,6 +18,10 @@ T_NUMBER = "NUMBER"
 T_BOPEN = "BRACKETOPEN"
 T_BCLOSE = "BRACKETCLOSE"
 T_ECHO = "PRINT-OUTPUT"
+T_ASSIGN = "ASSIGN"
+T_EOL = "SEMICOLON"
+T_CONCATE = "CONCATE"
+
 
 # IDENTIFIERS
 IDENTIFIERS = ["class", "function", "echo"]
@@ -34,19 +38,15 @@ with open("php.txt") as file_line:
 def pos(char):
     line_count = 1
     col_count = 1
-    words_per_line = []
-    for i in data_per_line:
-        words_per_line.append(i)
-    
+     
     # check if char is in a particular line 
-    for i in range(0, len(words_per_line)):
-        if char in words_per_line[i]:
+    for i in range(0, len(data_per_line)):
+        if char in data_per_line[i]:
             line_count += i
             
             # counting the columns by using the index of the char 
-            col_count += words_per_line[i].index(char) + 1 
+            col_count += data_per_line[i].index(char) 
     return line_count, col_count 
-
 
 class Token():
     def __init__(self, line, col, token_class, value=None):
@@ -56,8 +56,8 @@ class Token():
         self.value = value
 
     def __repr__(self):
-        if self.value: return f'{self.token_class}, {self.value}'
-        return f'{self.token_class}'
+        if self.value: return f'{self.line}, {self.col}, {self.token_class}, {self.value}'
+        return f'{self.line}, {self.col}, {self.token_class}'
 
 class Lexer():
     def __init__(self, text):
@@ -74,17 +74,14 @@ class Lexer():
         tokens = []
 
         while self.current_char != None:
+            line, col = pos(self.current_char)[0], pos(self.current_char)[1]
             if self.current_char == "+":
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                 tokens.append(Token(line, col, T_PLUS))
             elif self.current_char == "-":
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                 tokens.append(Token(line, col, T_MINUS))
             elif self.current_char == "*":
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                 tokens.append(Token(line, col, T_MUL))
             elif self.current_char == "/":
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                 tokens.append(Token(line, col, T_DIV))
 
             # php tags validation
@@ -95,7 +92,7 @@ class Lexer():
                     identifier += self.current_char
                     self.next()
                 if identifier == "<?php":
-                    line, col = pos(self.current_char)[0], pos(self.current_char)[1]
+                    line, col = pos(data, data.index(self.current_char))[0], pos(data, data.index(self.current_char))[1]
                     tokens.append(Token(line, col, T_PHPOPEN))
                 continue
             # close tag 
@@ -105,7 +102,7 @@ class Lexer():
                     identifier += self.current_char
                     self.next()
                 if identifier == "?>":
-                    line, col = pos(self.current_char)[0], pos(self.current_char)[1]
+                    line, col = pos(data, data.index(self.current_char))[0], pos(data, data.index(self.current_char))[1]
                     tokens.append(Token(line, col, T_PHPCLOSE))
 
 
@@ -120,35 +117,29 @@ class Lexer():
                     self.next()
                 # check for identifiers 
                 if identifier in IDENTIFIERS:
+                    # set the line and columns to word instead of character
+                    line, col = pos(data, data.index(self.current_char))[0], pos(data, data.index(self.current_char))[1]
                     # check for class or function identifiers 
                     if identifier == IDENTIFIERS[0]: 
-                        line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                         tokens.append(Token(line, col, T_CLASS))
                     elif identifier == IDENTIFIERS[1]:
-                        line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                         tokens.append(Token(line, col, T_FUNCTION))
                     elif identifier == IDENTIFIERS[2]:
-                        line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                         tokens.append(Token(line, col, T_ECHO))
                 else:
-                    line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                     tokens.append(Token(line, col, T_IDENTIFIER, identifier))
                 continue 
 
             # curly validation
             elif self.current_char == "{":
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                 tokens.append(Token(line, col, T_CURLYOPEN))
             elif self.current_char == "}":
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                 tokens.append(Token(line, col, T_CURLYCLOSE))
             
             # regular bracket validation
             elif self.current_char == "(":
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                 tokens.append(Token(line, col, T_BOPEN))
             elif self.current_char == ")":
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
                 tokens.append(Token(line, col, T_BCLOSE))
 
 
@@ -163,14 +154,44 @@ class Lexer():
                         decimal_count += 1
                     num += self.current_char
                     self.next()
-                line, col = pos(self.current_char)[0], pos(self.current_char)[1]
+                line, col = pos(data, data.index(num))[0], pos(data, data.index(num))[1]
                 tokens.append(Token(line, col, T_NUMBER, float(num)))
            
             # assign operator validation
             elif self.current_char == "=":
-                None 
+                tokens.append(Token(line, col, T_ASSIGN))
 
-             # whitespace addressing
+            # EOL validation
+            elif self.current_char == ";":
+                tokens.append(Token(line, col, T_EOL))
+            
+            # variable addressing
+            elif self.current_char == "$":
+                tokens.append(Token(line, col, T_VAR))
+
+            # string literal validation
+            elif self.current_char == '"':
+                identifier = '"'
+                self.next()
+                while str(self.current_char) != '"': 
+                    identifier += self.current_char
+                    self.next()
+                    if self.current_char == " ":
+                        self.current_char = "&nbsp"
+                    elif self.current_char == "'":
+                        self.current_char = "\\\'"
+                    elif self.current_char == "\\":
+                        self.current_char = "\\"
+                identifier += '"'
+                self.next()
+                line, col = pos(data, data.index(identifier))[0], pos(data, data.index(identifier))[1]
+                tokens.append(Token(line, col, T_LITERAL, identifier))
+                
+            # concatenate validation
+            elif self.current_char == ".":
+                tokens.append(Token(line, col, T_CONCATE))
+           
+                # whitespace addressing
             elif self.current_char == " ":
                 pass
 
