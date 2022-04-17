fb33f02d24f6057bbd155367765414afce90141b
Leon <doncorleone142857@gmail.com>
2022-04-17 17:26:34 +0700

    rename parser.py to parse.py so import can work. changed some things in main.py. Refactored parse.py so code is more clean.

--- Compilation Techniques/forums_lexer/parse.py
+++ Compilation Techniques/forums_lexer/parse.py
@@ -2,19 +2,40 @@ from lexer import Error
 from lexer import Lexer
 import re
 import string
+import TClass
+
+def check_open_close(fileName, tokens, T_OPEN, T_CLOSE, i, ignore_index):
+    if tokens[i][2] == T_OPEN and i not in ignore_index:
+        opentag = 1
+        closetag = 0
+        j = i + 1
+        recent = 0
+        while (j < len(tokens)):
+            if tokens[j][2] == T_OPEN:
+                recent = j
+                ignore_index.append(j)
+                opentag += 1
+            if tokens[j][2] == T_CLOSE:
+                closetag += 1
+            j += 1
+        if opentag != closetag:
+            Error(fileName, tokens[recent][0], tokens[recent][1], "Invalid Syntax").printErr()
+            return ignore_index, "break"
+        return ignore_index, ""
+    pass
 
 def Parser(fileName):
     # valid identifier followups
     ignore_index = []
-    identifier_dec = ["BRACKETOPEN", "SEMICOLON", "CURLYOPEN"]
-    identifier_op = ["ASSIGN", "ADD", "MINUS", "MUL", "DIV"]
+    identifier_dec = [TClass.T_BOPEN, TClass.T_EOL, TClass.T_CURLYOPEN]
+    identifier_op = [TClass.T_ASSIGN, TClass.T_PLUS, TClass.T_MINUS, TClass.T_MUL, TClass.T_DIV]
     escapes = ["\\", "'", '"', "$", "n", "t", "r"]
     valid_string = frozenset(string.ascii_letters + string.digits + '"#\\-$&=' + "'")
 
     # file 
     with open(fileName) as file:
         data = file.read()
-    
+
     tokens = Lexer(data).make_tokens()
 
     print("=================Lexer Result:=================\n")
@@ -30,79 +51,50 @@ def Parser(fileName):
         # error for invalid codes that i can think of
 
         # errors for syntax after identifier
-        if tokens[i][2] == "IDENTIFIER":
+        if tokens[i][2] == TClass.T_IDENTIFIER:
                 # errors when identifier name starts with numbers
                 if tokens[i][3][0] in "0123456789":
                     Error(fileName, tokens[i][0], tokens[i][1], "Invalid Character Error").printErr()
                     break
-                
+
                 # check if the syntax after identifier name is invalid
                 if not ((tokens[i+1][2] in identifier_dec) or (tokens[i+1][2] in identifier_op)):
                     Error(fileName, tokens[i+1][0], tokens[i+1][1], "Invalid Syntax").printErr()
                     break
-        
-        # errors related to incomplete opening or closing statements
-        if tokens[i][2] == "PHPOPENTAG" and i not in ignore_index:
-            opentag = 1
-            closetag = 0
-            j = i + 1
-            while (j < len(tokens)):
-                if tokens[j][2] == "PHPOPENTAG":
-                    recent = j
-                    ignore_index.append(j)
-                    opentag += 1
-                if tokens[j][2] == "PHPCLOSETAG":
-                    closetag += 1
-                j += 1
-            if opentag != closetag:
-                Error(fileName, tokens[recent][0], tokens[recent][1], "Invalid Syntax").printErr()
-                break
-        
-        if tokens[i][2] == "CURLYOPEN" and i not in ignore_index:
-            opentag = 1
-            closetag = 0
-            j = i + 1
-            while (j < len(tokens)):
-                if tokens[j][2] == "CURLYOPEN":
-                    recent = j
-                    ignore_index.append(j)
-                    opentag += 1
-                if tokens[j][2] == "CURLYCLOSE":
-                    closetag += 1
-                j += 1
-            if opentag != closetag:
-                Error(fileName, tokens[recent][0], tokens[recent][1], "Invalid Syntax").printErr()
-                break
 
-        if tokens[i][2] == "BRACKETOPEN" and i not in ignore_index:
-            opentag = 1
-            closetag = 0
-            j = i + 1
-            while (j < len(tokens)):
-                if tokens[j][2] == "BRACKETOPEN":
-                    recent = j
-                    ignore_index.append(j)
-                    opentag += 1
-                if tokens[j][2] == "BRACKETCLOSE":
-                    closetag += 1
-                j += 1
-            if opentag != closetag:
-                Error(fileName, tokens[recent][0], tokens[recent][1], "Invalid Syntax").printErr()
-                break
+
+        # errors related to incomplete opening or closing statements
+        result = check_open_close(fileName, tokens, TClass.T_PHPOPEN, TClass.T_PHPCLOSE, i, ignore_index)
+        if result is not None:
+            ignore_index = result[0]
+            if result[1] != "":
+                eval(result[1])
+
+        result = check_open_close(fileName, tokens, TClass.T_CURLYOPEN, TClass.T_CURLYCLOSE, i, ignore_index)
+        if result is not None:
+            ignore_index = result[0]
+            if result[1] != "":
+                eval(result[1])
+
+        result = check_open_close(fileName, tokens, TClass.T_BOPEN, TClass.T_BCLOSE, i, ignore_index)
+        if result is not None:
+            ignore_index = result[0]
+            if result[1] != "":
+                eval(result[1])
 
         # error for multi-line comment
-        if tokens[i][2] == "MULTIOPEN":
+        if tokens[i][2] == TClass.T_MULTIOPEN:
             j = i + 1
             found = False
             while (j < len(tokens) and not found):
-                if tokens[j][2] == "MULTICLOSE":
+                if tokens[j][2] == TClass.T_MULTICLOSE:
                     found = True
             if not found:
                 Error(fileName, tokens[j-1][0], tokens[j-1][1], "Multi Comment Not Closed").printErr()
                 break
 
         # error for string literal not ended with double quotes.
-        if tokens[i][2] == "STRING-LITERAL":
+        if tokens[i][2] == TClass.T_LITERAL:
             identifier = tokens[i][3]
             if not (identifier.startswith('"') and identifier.endswith('"')):
                 Error(fileName, tokens[i][0], tokens[i][1], "String Literal Not Closed With Duoble Quotes").printErr()
@@ -112,7 +104,7 @@ def Parser(fileName):
         # i have no idea lmao
 
         # error for invalid string characters
-        if tokens[i][2] == "STRING-LITERAL":
+        if tokens[i][2] == TClass.T_LITERAL:
             identifier = set(tokens[i][3])
             # returns true if there are invalid string character inside the identifier
             valid =  not identifier <= valid_string
